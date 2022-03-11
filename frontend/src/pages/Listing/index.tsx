d7cd7820f7edf38b450ed6ece99033a6d8722996
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-03-11 07:41:40 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,18 +2,26 @@ import axios from 'axios';
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
 import { BASE_URL } from 'utils/requests';
+import { useState, useEffect } from 'react'
+import { MoviePage } from 'types/movie';
 
 function Listing(){
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data)
-    });
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            const data = response.data as MoviePage
+            console.log(data)
+            setPageNumber(data.number)
+        });
+    }, [])
 
     return(
         <>
             <Pagination />
+            <p>{pageNumber}</p>
             <div className='container'>
                 <div className='row'>
                     <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
